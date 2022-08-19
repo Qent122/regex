91fa530b9e4eb1cbf133d6997b0a3dce5d0e2b44
Quentin Cliquot <quentincliquot@gmail.com>
2022-08-19 10:13:43 +0200

    view preg_match and preg_match_all

--- index.php
+++ index.php
@@ -1,19 +1,32 @@
 <!DOCTYPE html>
 <html lang="fr">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Regex</title>
 </head>
+
 <body>
     <h1>Je fait du Regex</h1>
-<?php
+    <?php
 
-$masque='/michel/'
+    $masque = '/m/i'; //La lettre 'i' rend le masque insencible a la casse
+    $chaine = 'michel maman';
 
-?>
+    if (preg_match($masque, $chaine)) {
+        echo "<p>J'ai trouver l'occurence</p>";
+        echo "<p>Le cacartere rechercher est present : ". preg_match_all($masque,$chaine)." fois </p>";
+
+
+    } else {
+        echo "J'ai rien trouvé";
+    }
+
+
+    ?>
 
 </body>
-</html>
 
+</html>
