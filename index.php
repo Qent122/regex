263dd5a59a3c748d7a2db34f5016e3daaf2edaae
Quentin Cliquot <quentincliquot@gmail.com>
2022-08-19 11:26:58 +0200

    view preg_grep

--- index.php
+++ index.php
@@ -12,17 +12,47 @@
     <h1>Je fait du Regex</h1>
     <?php
 
-    $masque = '/m/i'; //La lettre 'i' rend le masque insencible a la casse
-    $chaine = 'michel maman';
+function dump($data){
+    echo "<pre>";
+    var_dump($data);
+    echo "</pre>";
+    echo "<br>";
+}
+function voir($data){
+    echo "<p>$data</p>";
+}
 
-    if (preg_match($masque, $chaine)) {
-        echo "<p>J'ai trouver l'occurence</p>";
-        echo "<p>Le cacartere rechercher est present : ". preg_match_all($masque,$chaine)." fois </p>";
+    $masque = '/Michel/i'; //La lettre 'i' rend le masque insencible a la casse
+    $chaine = "Bonjour, je m'appelle Michel";
+    $tablo=['Michel', 'Jean-Michel','Pierre', 'Paul-Michel','Jack'];
+    $match_all=[];
+
+    $resultat= preg_grep($masque,$tablo);
+    dump($resultat);
 
 
-    } else {
-        echo "J'ai rien trouvé";
-    }
+
+    // preg_match($masque,$chaine,$match);
+    // preg_match_all($masque,$chaine,$match_all);
+
+    // dump($match);
+
+    // dump($match_all);
+
+    // $match_all2=[];
+    // $valeur=preg_match_all($masque,$chaine,$match_all2, PREG_PATTERN_ORDER,3);
+    // voir("il y a $valeur fois la lettre recherchées");
+
+
+
+    // if (preg_match($masque, $chaine)) {
+    //     echo "<p>J'ai trouver l'occurence</p>";
+    //     echo "<p>Le cacartere rechercher est present : ". preg_match_all($masque,$chaine)." fois </p>";
+
+
+    // } else {
+    //     echo "J'ai rien trouvé";
+    // }
 
 
     ?>
